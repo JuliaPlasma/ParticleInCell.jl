c7ec097b08690184929b8d89d05859c25310d024
Luke Adams <luke@lukeclydeadams.com>
2023-05-13 22:12:40 -0600

    plan ffts

--- src/poisson.jl
+++ src/poisson.jl
@@ -1,9 +1,11 @@
-struct PoissonSolveFFT{T, D, G, F <: AbstractField} <: AbstractSimulationStep
+struct PoissonSolveFFT{T, D, G, P, F <: AbstractField} <: AbstractSimulationStep
     rho::F
     phi::F
 
     Ksq_inv::Array{T, D}
     ft_vector::Array{Complex{T}, D}
+
+    fft_plan::P
 
     function PoissonSolveFFT(rho::F, phi::F) where {T, D, G, F <: AbstractField{T, D, G}}
         # This restriction could possibly be relaxed to just require compatible grids...
@@ -30,39 +32,17 @@ struct PoissonSolveFFT{T, D, G, F <: AbstractField} <: AbstractSimulationStep
             Ksq_inv[I] = prod(inv_Ksqs)
         end
 
-        new{T, D, G, F}(rho, phi, Ksq_inv, ft_vector)
+        fft_plan = plan_fft!(ft_vector)
+
+        new{T, D, G, typeof(fft_plan), F}(
+            rho, phi, Ksq_inv, ft_vector, fft_plan)
     end
 end
 
 function step!(::Any, step::PoissonSolveFFT)
-    # TODO...
     step.ft_vector .= view(step.rho.values, eachindex(step.rho))
-    FFTW.fft!(step.ft_vector)
+    step.fft_plan * step.ft_vector
     step.ft_vector .= step.ft_vector .* step.Ksq_inv
-    FFTW.ifft!(step.ft_vector)
+    inv(step.fft_plan) * step.ft_vector
     view(step.phi.values, eachindex(step.rho)) .= real.(step.ft_vector)
 end
-
-# for i in 1:round(Int, 1 + num_cells / 2)
-#     k = 2 * pi * i / sim_length
-#     # grid_angle can alternatively be computed as i * pi / num_cells. I
-#     # don't do this for intuitions sake.
-#     grid_angle = k * cell_length / 2
-#     step.ksq_inv[i] = (cell_length / (2 * sin(grid_angle)))^2 / step.epsilon_0
-# end
-
-# # Modifiy ft_vector to get phi(k)
-# # This mode should be zero if the simulation cell is charge neutral. We
-# # assume that it is here.
-# step.ft_vector[1] = 0
-
-# # The modes 2:num_cells (of which there are num_cells - 1) are paired in conjugate
-# # pairs so rhok[2] = complex_conjugate(rhok[end]) and so on. Thus we need
-# # to multiply both by the same scaling factor.
-# for i in 1:round(Int, num_cells / 2)
-#     k1 = i + 1
-#     k2 = num_cells + 1 - i
-#     step.ft_vector[k1] = step.ksq_inv[i] * step.ft_vector[k1]
-#     k1 == k2 && break
-#     step.ft_vector[k2] = step.ksq_inv[i] * step.ft_vector[k2]
-# end
