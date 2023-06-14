77815537a99a9d82dff50e565e48f03573160116
Luke Adams <luke@lukeclydeadams.com>
2023-06-13 23:13:10 -0600

    checkpoint
    Still need to make the interpolation work with multidimensional
    simulations, which should involve adding a loop to the generated
    b-spline function.
    
    Still need to refactor the step! functions to use the grid coordinates,
    and enable interpolations from edge and face fields.

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -1,120 +1,3 @@
-struct BSplineChargeInterpolation{S,F,IF} <: AbstractSimulationStep
-    species::S
-    rho::F
-
-    bspline_order::Int
-    interp_width::Int
-
-    interp_func::IF
-
-    function BSplineChargeInterpolation(species::S, rho::F, bspline_order::Int) where {S,F}
-        @assert rho.offset == node
-
-        interp_width = div(bspline_order, 2) + 1
-
-        # TODO: calculate b-spline orders in general...
-        @assert bspline_order == 1
-        interp_func = function (xs)
-            weight = one(eltype(xs))
-            for x in xs
-                if abs(x) > 1
-                    return zero(eltype(xs))
-                elseif x < 0
-                    weight *= 1 + x
-                else
-                    weight *= 1 - x
-                end
-            end
-            return weight
-        end
-
-        new{S,F,typeof(interp_func)}(species, rho, bspline_order, interp_width, interp_func)
-    end
-end
-
-function step!(step::BSplineChargeInterpolation)
-    grid = step.rho.grid
-    cell_volume = prod(cell_lengths(grid))
-
-    for i in eachindex(step.species.positions)
-        # Find which cell the particle is in, and create a CartesianIndices
-        # object that extends +/- interp_width in all directions
-        Is = phys_coords_to_cell_index_ittr(
-            step.rho,
-            step.species.positions[i],
-            step.interp_width,
-        )
-
-        # Iterate over nodes within the stencil, and compute the corresponding
-        # charge for each node
-        for I in Is
-            step.rho.values[I] +=
-                step.species.charge * step.species.weights[i] / cell_volume *
-                step.interp_func(interp_dist(step.rho, I, step.species.positions[i]))
-        end
-    end
-end
-
-struct BSplineFieldInterpolation{S,F,IF} <: AbstractSimulationStep
-    species::S
-    elec::F
-
-    bspline_order::Int
-    interp_width::Int
-
-    interp_func::IF
-
-    function BSplineFieldInterpolation(species::S, elec::F, bspline_order::Int) where {S,F}
-        # TODO: support interpolations from edge/face fields
-        @assert elec.offset == node
-
-        interp_width = div(bspline_order, 2) + 1
-
-        # TODO: calculate b-spline orders in general...
-        @assert bspline_order == 1
-        interp_func = function (xs)
-            weight = one(eltype(xs))
-            for x in xs
-                if abs(x) > 1
-                    return zero(eltype(xs))
-                elseif x < 0
-                    weight *= 1 + x
-                else
-                    weight *= 1 - x
-                end
-            end
-            return weight
-        end
-
-        new{S,F,typeof(interp_func)}(
-            species,
-            elec,
-            bspline_order,
-            interp_width,
-            interp_func,
-        )
-    end
-end
-
-function step!(step::BSplineFieldInterpolation)
-    for n in eachindex(step.species.positions)
-        # Find which cell the particle is in, and create a CartesianIndices
-        # object that extends +/- interp_width in all directions
-        Is = phys_coords_to_cell_index_ittr(
-            step.elec,
-            step.species.positions[n],
-            step.interp_width,
-        )
-
-        for I in Is
-            step.species.forces[n] =
-                step.species.forces[n] .+
-                step.species.charge .* step.species.weights[n] .* step.elec.values[I] .*
-                step.interp_func(interp_dist(step.elec, I, step.species.positions[n]))
-        end
-    end
-end
-
 """
     compute_knots(degree)
 
@@ -137,11 +20,11 @@ function compute_bspline_coeffs(degree, T = Float64)
     segments = fill(zeros(T, degree + 1), degree + 1)
     lower_segs = compute_bspline_coeffs(degree - 1, T)
 
-    # Special case the two end segments, which only depend on one lower order segment
+    # Special case the two end segments, which only depend on one lower degree segment
     segments[1] = poly_integrate(binomial_expand(lower_segs[1], 0.5))
     segments[end] = -1 .* poly_integrate(binomial_expand(lower_segs[end], -0.5))
 
-    # Compute the middle segments as a function of the two neighboring lower order segments
+    # Compute the middle segments as a function of the two neighboring lower degree segments
     for i = 2:length(segments)-1
         shifted_splines =
             binomial_expand(lower_segs[i], +0.5) - binomial_expand(lower_segs[i-1], -0.5)
@@ -187,4 +70,114 @@ function binomial_expand(coeffs, const_term)
         new_coeffs[j] += coeffs[i] * binomial(i - 1, j - 1) * const_term^(i - j)
     end
     return new_coeffs
+end
+
+function create_bspline_interp_expr(degree)
+    knots = compute_knots(degree)
+    coeffs = compute_bspline_coeffs(degree)
+    exprs = Expr[]
+    push!(exprs, quote
+        if abs(xs) > $(knots[end])
+            return 0
+        end
+    end)
+    for i in 2:length(knots)
+        push!(exprs, quote
+            if xs <= $(knots[i])
+                return poly_eval($(coeffs[i - 1]), xs)
+            end
+        end)
+    end
+    stripped_exprs = Expr[]
+    for ex in exprs
+        push!(stripped_exprs, ex.args[2])
+    end
+    return Expr(:function, Expr(:tuple, :xs), Expr(:block, stripped_exprs...))
+end
+
+struct BSplineChargeInterpolation{S,F,IF} <: AbstractSimulationStep
+    species::S
+    rho::F
+
+    bspline_degree::Int
+    interp_width::Int
+
+    interp_func::IF
+
+    function BSplineChargeInterpolation(species::S, rho::F, bspline_degree::Int) where {S,F}
+        @assert rho.offset == node
+
+        interp_width = div(bspline_degree, 2) + 1
+        interp_func = eval(create_bspline_interp_expr(bspline_degree))
+
+        new{S,F,typeof(interp_func)}(species, rho, bspline_degree, interp_width, interp_func)
+    end
+end
+
+function step!(step::BSplineChargeInterpolation)
+    grid = step.rho.grid
+    cell_volume = prod(cell_lengths(grid))
+
+    for i in eachindex(step.species.positions)
+        # Find which cell the particle is in, and create a CartesianIndices
+        # object that extends +/- interp_width in all directions
+        Is = phys_coords_to_cell_index_ittr(
+            step.rho,
+            step.species.positions[i],
+            step.interp_width,
+        )
+
+        # Iterate over nodes within the stencil, and compute the corresponding
+        # charge for each node
+        for I in Is
+            step.rho.values[I] +=
+                step.species.charge * step.species.weights[i] / cell_volume *
+                step.interp_func(interp_dist(step.rho, I, step.species.positions[i])[1])
+        end
+    end
+end
+
+struct BSplineFieldInterpolation{S,F,IF} <: AbstractSimulationStep
+    species::S
+    elec::F
+
+    bspline_degree::Int
+    interp_width::Int
+
+    interp_func::IF
+
+    function BSplineFieldInterpolation(species::S, elec::F, bspline_degree::Int) where {S,F}
+        # TODO: support interpolations from edge/face fields
+        @assert elec.offset == node
+
+        interp_width = div(bspline_degree, 2) + 1
+        interp_func = eval(create_bspline_interp_expr(bspline_degree))
+
+        new{S,F,typeof(interp_func)}(
+            species,
+            elec,
+            bspline_degree,
+            interp_width,
+            interp_func,
+        )
+    end
+end
+
+function step!(step::BSplineFieldInterpolation)
+    for n in eachindex(step.species.positions)
+        # Find which cell the particle is in, and create a CartesianIndices
+        # object that extends +/- interp_width in all directions
+        Is = phys_coords_to_cell_index_ittr(
+            step.elec,
+            step.species.positions[n],
+            step.interp_width,
+        )
+
+        for I in Is
+            step.species.forces[n] =
+                step.species.forces[n] .+
+                step.species.charge .* step.species.weights[n] .* step.elec.values[I] .*
+                step.interp_func(interp_dist(step.elec, I, step.species.positions[n])[1])
+        end
+    end
 end
