7bf5c3f1a1f549ae75c3a50cf4d35b68cd35047c
Luke Adams <luke@lukeclydeadams.com>
2023-06-13 22:16:06 -0600

    add groundwork for arbitrary order b-spline generation

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -114,3 +114,77 @@ function step!(step::BSplineFieldInterpolation)
         end
     end
 end
+
+"""
+    compute_knots(degree)
+
+Returns a vector of the knot locations for a polynomial b-spline with `degree`.
+"""
+function compute_knots(degree)
+    collect(range(0, degree + 1)) .- ((degree + 1) / 2)
+end
+
+"""
+    compute_bspline_coeffs(degree, [T])
+
+Returns a vector of vectors of the coefficients for the b-spline with
+polynomial `degree`, and unit-spaced knots, centered at zero.
+"""
+function compute_bspline_coeffs(degree, T = Float64)
+    degree == 0 && return [[one(T)]]
+
+    knots = compute_knots(degree)
+    segments = fill(zeros(T, degree + 1), degree + 1)
+    lower_segs = compute_bspline_coeffs(degree - 1, T)
+
+    # Special case the two end segments, which only depend on one lower order segment
+    segments[1] = poly_integrate(binomial_expand(lower_segs[1], 0.5))
+    segments[end] = -1 .* poly_integrate(binomial_expand(lower_segs[end], -0.5))
+
+    # Compute the middle segments as a function of the two neighboring lower order segments
+    for i = 2:length(segments)-1
+        shifted_splines =
+            binomial_expand(lower_segs[i], +0.5) - binomial_expand(lower_segs[i-1], -0.5)
+        segments[i] = poly_integrate(shifted_splines)
+    end
+
+    # Adjust the constant terms to ensure continuity
+    segments[1][1] = 0 - poly_eval(segments[1], knots[1])
+    for i = 2:length(segments)
+        segments[i][1] =
+            poly_eval(segments[i-1], knots[i]) - poly_eval(segments[i], knots[i])
+    end
+
+    return segments
+end
+
+# Integrate the polynomial specified by coeffs using the standard power
+# integration rule. Note that the constant term is undetermined. Thus routine
+# sets the constant equal to zero.
+function poly_integrate(coeffs)
+    new_coeffs = zeros(eltype(coeffs), length(coeffs) + 1)
+    for i = 1:length(coeffs)
+        new_coeffs[i+1] = coeffs[i] / i
+    end
+    return new_coeffs
+end
+
+function poly_eval(coeffs, x)
+    result = zero(x)
+    xp = one(x)
+    for c in coeffs
+        result += c * xp
+        xp *= x
+    end
+    return result
+end
+
+# If coeffs = [c_0, c_1, ..., c_n], then create a polynomial that is the
+# expansion of sum_n c_n (x + const_term)^(n-1)
+function binomial_expand(coeffs, const_term)
+    new_coeffs = zeros(eltype(coeffs), length(coeffs))
+    for i = 1:length(coeffs), j = 1:i
+        new_coeffs[j] += coeffs[i] * binomial(i - 1, j - 1) * const_term^(i - j)
+    end
+    return new_coeffs
+end
--- src/interpolation_test.jl
+++ src/interpolation_test.jl
@@ -19,3 +19,48 @@
     step!(bs_interp)
     @test sum_charge(rho) == 1
 end
+
+@testitem "b-spline basics" begin
+    import ParticleInCell2:
+        poly_integrate, binomial_expand, compute_knots, compute_bspline_coeffs
+
+    @test poly_integrate([1.0]) == [0, 1]
+    @test poly_integrate([3.0, 4.0]) == [0, 3, 2]
+
+    @test binomial_expand([0.0, 1], 0.5) == [0.5, 1.0]
+    @test binomial_expand([1.0, 0.0, 1], 0.5) == [1.25, 1.0, 1.0]
+
+    @test compute_knots(0) == [-0.5, 0.5]
+    @test compute_knots(1) == [-1, 0, 1]
+    @test compute_knots(2) == [-1.5, -0.5, 0.5, 1.5]
+    @test compute_knots(3) == [-2, -1, 0, 1, 2]
+
+    # Polynomial coefficients can be computed from
+    # https://en.wikipedia.org/wiki/Irwin–Hall_distribution#Special_cases
+    # by translating the basis functions so that they are centered at x=0.
+    @test isapprox(compute_bspline_coeffs(0), [[1]])
+    @test isapprox(compute_bspline_coeffs(1), [[1, 1], [1, -1]])
+    @test isapprox(
+        compute_bspline_coeffs(2),
+        [[9 / 8, 3 / 2, 1 / 2], [3 / 4, 0, -1], [9 / 8, -3 / 2, 1 / 2]],
+    )
+    @test isapprox(
+        compute_bspline_coeffs(3),
+        [
+            [4 / 3, 2, 1, 1 / 6],
+            [2 / 3, 0, -1, -1 / 2],
+            [2 / 3, 0, -1, 1 / 2],
+            [4 / 3, -2, 1, -1 / 6],
+        ],
+    )
+    @test isapprox(
+        compute_bspline_coeffs(4),
+        [
+            [625 / 384, 125 / 48, 25 / 16, 5 / 12, 1 / 24],
+            [55 / 96, -5 / 24, -5 / 4, -5 / 6, -1 / 6],
+            [115 / 192, 0, -5 / 8, 0, 1 / 4],
+            [55 / 96, 5 / 24, -5 / 4, 5 / 6, -1 / 6],
+            [625 / 384, -125 / 48, 25 / 16, -5 / 12, 1 / 24],
+        ],
+    )
+end
