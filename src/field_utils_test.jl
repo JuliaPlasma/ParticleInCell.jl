87a9265c95eec53e4f552392aa075e1c512945bf
Luke Adams <luke@lukeclydeadams.com>
2023-08-01 14:00:28 -0600

    add field utils tests

---
+++ src/field_utils_test.jl
@@ -0,0 +1,50 @@
+@testitem "FiniteDifferenceToEdges" tags = [:unit] begin
+    @testset "1D" begin
+        grid = UniformCartesianGrid((0.0,), (1.0,), (10,), (false,))
+        nodal_field = Field(grid, ParticleInCell2.node, 1)
+        edge_field = Field(grid, ParticleInCell2.edge, 1)
+
+        step = FiniteDifferenceToEdges(nodal_field, edge_field)
+        @test step.edge_lengths == (0.1,)
+
+        nodal_field[:] .= 1.0
+        step!(step)
+        @test all(edge_field[:] .== 0)
+
+        nodal_field[:] = collect(range(0, 10))
+        step!(step)
+        @test edge_field[5] .== -10
+        @test edge_field[11] .== 0
+    end
+end
+
+@testitem "AverageEdgesToNodes" tags = [:unit] begin
+    @testset "1D" begin
+        grid = UniformCartesianGrid((0.0,), (1.0,), (10,), (false,))
+        nodal_field = Field(grid, ParticleInCell2.node, 1, 1)
+        edge_field = Field(grid, ParticleInCell2.edge, 1, 1)
+
+        step = AverageEdgesToNodes(edge_field, nodal_field)
+
+        edge_field.values[:] .= 2
+        step!(step)
+        @test nodal_field[5] == 2
+    end
+end
+
+@testitem "CommunicateGuardCells" tags = [:unit] begin
+    @testset "1D" begin
+        grid = UniformCartesianGrid((0.0,), (1.0,), (10,), (false,))
+        field = Field(grid, ParticleInCell2.node, 1)
+
+        step = CommunicateGuardCells(field)
+
+        field[1] = 1.0
+        field[11] = 0.0
+        @test field[1] == 1.0
+        @test field[11] == 0.0
+        step!(step)
+        @test field[1] == 1.0
+        @test field[11] == 1.0
+    end
+end
