50a2119bbf297ae99843177fbeb85ccd6d15aa46
Luke Adams <luke@lukeclydeadams.com>
2023-08-04 22:25:14 -0600

    implement boris push and add basic tests

--- src/species.jl
+++ src/species.jl
@@ -4,9 +4,9 @@ abstract type AbstractSpecies end
 Stores the positions and momentums of particles that share a common charge and
 mass. Each particle can have a different weight.
 """
-struct Species{D,T} <: AbstractSpecies
+struct Species{D,V,T} <: AbstractSpecies
     positions::Vector{SVector{D,T}}
-    momentums::Vector{SVector{D,T}}
+    momentums::Vector{SVector{V,T}}
     weights::Vector{T}
     charge::T
     mass::T
