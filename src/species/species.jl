4a8a0d1f3655f97d96dfd8ba3c9ca3c5b65f2c87
Luke Adams <luke@lukeclydeadams.com>
2023-08-10 14:04:48 -0600

    rearange species code and add interface

---
+++ src/species/species.jl
@@ -0,0 +1,105 @@
+"""
+Subtypes of `AbstractSpecies` represent a group of macroparticles with a single physical
+charge and mass. Each particle of a species can optionally represent a different number of
+physical particles.
+"""
+abstract type AbstractSpecies end
+
+"""
+    particle_charge(species, idx)
+
+Returns the charge of the macroparticle of `species` with index `idx`. See also
+[`physical_charge`](@ref).
+"""
+function particle_charge end
+
+"""
+    physical_charge(species, idx)
+
+Returns the charge of a physical particle represented by the macroparticle of `species` with
+index `idx`. See also [`particle_charge`](@ref).
+"""
+function physical_charge end
+
+"""
+    particle_mass(species, idx)
+
+Returns the mass of the macroparticle of `species` with index `idx`. See also
+[`physical_mass`](@ref).
+"""
+function particle_mass end
+
+"""
+    physical_mass(species, idx)
+
+Returns the mass of a physical particle represented by the macroparticle of `species` with
+index `idx`. See also [`particle_charge`](@ref).
+"""
+function physical_mass end
+
+"""
+    particle_weight(species, idx)
+
+Returns the number of  physical particles represented by the macroparticle of `species` with
+index `idx`.
+"""
+function particle_weight end
+
+"""
+    particle_position(species, idx)
+
+Returns the position of the macroparticle of `species` with index `idx`. See also
+[`particle_momentum`](@ref) and [`particle_position!`](@ref).
+"""
+function particle_position end
+
+"""
+    particle_position!(species, idx, value)
+
+Sets the position of the macroparticle of `species` with index `idx` to `value`. See also
+[`particle_momentum!`](@ref) and [`particle_position`](@ref).
+"""
+function particle_position! end
+
+"""
+    particle_momentum(species, idx)
+
+Returns the momentum of the macroparticle of `species` with index `idx`. See also
+[`particle_momentum!`](@ref), [`particle_velocity`](@ref), and [`physical_momentum`](@ref).
+"""
+function particle_momentum end
+
+"""
+    particle_momentum!(species, idx, value)
+
+Sets the momentum of the macroparticle of `species` with index `idx` to `value`. See also
+[`particle_momentum`](@ref), [`particle_velocity`](@ref), and [`physical_momentum`](@ref).
+"""
+function particle_momentum! end
+
+"""
+    particle_velocity(species, idx)
+
+Returns the velocity of the macroparticle of `species` with index `idx`. See also
+[`particle_momentum`](@ref), [`particle_momentum!`](@ref), and [`physical_momentum`](@ref).
+"""
+function particle_velocity end
+
+"""
+    physical_momentum(species, idx)
+
+Returns the momentum of a physical particle represented by the macroparticle of `species`
+with index `idx`. See also [`particle_momentum`](@ref), [`particle_momentum!`](@ref), and
+[`particle_velocity`](@ref).
+"""
+function physical_momentum end
+
+"""
+    eachindex(species)
+
+Returns an iterator to the index of each of the macroparticles contained in `species`.
+"""
+function eachindex end
+
+include("variable_weight.jl")
+include("species_utils.jl")
--- src/species/species_utils.jl
+++ src/species/species_utils.jl
@@ -1,42 +1,3 @@
-abstract type AbstractSpecies end
-
-"""
-    Species(positions::Matrix, momentums::Matrix, weight::Vector charge, mass)
-
-Stores the positions and momentums of particles that share a common charge and mass. Each
-particle can have a different weight, and the momentum space can have a larger dimension
-than the position space.
-"""
-@struct_hash_equal struct Species{D,V,T} <: AbstractSpecies
-    positions::Vector{SVector{D,T}}
-    momentums::Vector{SVector{V,T}}
-    weights::Vector{T}
-    charge::T
-    mass::T
-end
-
-function Species(
-    positions::Matrix{T},
-    momentums::Matrix{T},
-    weights::Vector{T},
-    charge::T,
-    mass::T,
-) where {T}
-    D, pos_nparticles = length.(axes(positions))
-    V, mom_nparticles = length.(axes(momentums))
-
-    @assert pos_nparticles == mom_nparticles == length(weights)
-
-    pos_vec = Vector{SVector{D,T}}(undef, pos_nparticles)
-    mom_vec = Vector{SVector{V,T}}(undef, pos_nparticles)
-    for i = 1:pos_nparticles
-        pos_vec[i] = SVector(positions[:, i]...)
-        mom_vec[i] = SVector(momentums[:, i]...)
-    end
-
-    return Species{D,V,T}(pos_vec, mom_vec, weights, charge, mass)
-end
-
 """
     electrons(positions::Matrix, momentums::Matrix, weights::Vector)
     electrons(positions::Matrix, momentums::Matrix, weight)
@@ -64,7 +25,6 @@ function electrons(positions::Vector{T}, momentums::Vector{T}, weights::Vector{T
         9.1093837015e-31,
     )
 end
-
 
 function electrons(positions::Vector{T}, momentums::Vector{T}, weight::T) where {T}
     weights = fill(weight, length(positions))
