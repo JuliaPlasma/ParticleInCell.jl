8e1ccec516563294e4d7ffc34d10da55a081d13d
Documenter.jl <documenter@juliadocs.github.io>
2024-03-22 21:04:15 +0000

    build based on 8cd1188

--- dev/examples/tutorial/langmuir_oscillation.jl
+++ dev/examples/tutorial/langmuir_oscillation.jl
@@ -1,7 +1,15 @@
 using ParticleInCell
 using CairoMakie
 CairoMakie.activate!(type = "svg") #hide
-set_theme!(theme_light()) #hide
+docs_theme = Theme(
+    fontsize = 20,
+    fonts = (; regular = "Lato", bold = "Lato Bold"),
+    palette = (color = [:black, :red, :blue],),
+    Axis = (xgridvisible = false, ygridvisible = false),
+    Scatter = (; cycle = [:color]),
+    Lines = (; cycle = [:color]),
+)
+set_theme!(docs_theme) #hide
 
 sim_length = 1.0
 number_density = 1e14
@@ -15,15 +23,16 @@ amplitude = 1e3
 elec_mass = 9e-31
 momentums = (particles_per_macro * elec_mass * amplitude) .* sin.(positions .* k);
 
-scatter(
-    positions,
-    momentums;
-    axis = (;
-        title = "Electron phase space",
-        xlabel = "Position (m)",
-        ylabel = "Momentum (kg m / s)",
-    ),
+fig = Figure(size = (1000, 400))
+ax = Axis(
+    fig[1, 1],
+    title = "Electron phase space",
+    xlabel = "Position (m)",
+    ylabel = "Momentum (arb. units)",
+    limits = ((0, 1), nothing),
 )
+scatter!(ax, positions, momentums ./ maximum(momentums))
+fig
 
 electrons = ParticleInCell.electrons(positions, momentums, particles_per_macro);
 
@@ -38,13 +47,13 @@ elec_mass = 9e-31
 expected_plasma_freq = sqrt(number_density * elec_charge^2 / elec_mass / epsilon_0)
 expected_plasma_period = 2pi / expected_plasma_freq
 
-dt = 5e-11
+dt = 0.01 / expected_plasma_freq
 
-sim, fields = create_electrostatic_simulation(grid, [electrons], dt)
+sim, fields = create_electrostatic_simulation(grid, [electrons], dt);
 
-Enode = fields[:Enode]
+Enode = fields[:Enode];
 
-n_steps = 1000
+n_steps = 10000
 
 electric_field_energy = Vector{Float64}(undef, n_steps)
 
@@ -58,83 +67,95 @@ for n = 1:n_steps
     step!(sim)
 end
 
-times = collect(range(1, n_steps)) .* dt
-lines(
-    times,
-    electric_field_energy;
-    axis = (; title = "Electric Field Energy", xlabel = "Time (s)", ylabel = "Energy"),
+normalized_times = collect(range(1, n_steps)) .* dt .* expected_plasma_freq
+fig = Figure(size = (1000, 400))
+ax = Axis(
+    fig[1, 1],
+    title = "Electric Field Energy",
+    xlabel = "Normalized Time",
+    ylabel = "Energy",
+    limits = ((0, 100), nothing),
 )
+lines!(normalized_times, electric_field_energy)
+fig
 
 using FFTW
 
 freqs = fftfreq(n_steps, 1 / dt) .* 2pi
 freq_amps = abs.(fft(electric_field_energy))
 
-lines(
-    freqs,
-    freq_amps;
-    axis = (;
-        title = "Electric Field Energy Frequency Spectrum",
-        xlabel = "Frequency (1/s)",
-        ylabel = "Amplitude",
-    ),
+fig = Figure(size = (1000, 400))
+ax = Axis(
+    fig[1, 1],
+    title = "Electric Field Energy Spectrum",
+    xlabel = "Frequency",
+    ylabel = "Amplitude",
 )
+lines!(ax, freqs, freq_amps)
+fig
 
-cutoff_index = round(Int, n_steps * 0.05)
-lines(
-    freqs[1:cutoff_index],
-    freq_amps[1:cutoff_index];
-    axis = (;
-        title = "Electric Field Energy Frequency Spectrum",
-        xlabel = "Frequency (1/s)",
-        ylabel = "Amplitude",
-    ),
+cutoff_index = round(Int, n_steps * 0.005)
+fig = Figure(size = (1000, 400))
+ax = Axis(
+    fig[1, 1],
+    title = "Electric Field Energy Spectrum",
+    xlabel = "Frequency",
+    ylabel = "Amplitude",
 )
+lines!(ax, freqs[1:cutoff_index], freq_amps[1:cutoff_index])
+fig
 
-freq_amps[1] = 0
+freq_amps .= ifelse.(freqs .< 5e8, 0, freq_amps)
 max_index = findmax(freq_amps)[2]
 max_freq = freqs[max_index]
 
 # Divide by 2 because the electric field energy goes through a maximum twice
 # per plasma oscillation, and take the absolute value because we don't care
 # about the phase of the oscillation.
-plasma_freq = abs(max_freq / 2)
+measured_plasma_freq = abs(max_freq / 2)
 
-relative_error = (plasma_freq - expected_plasma_freq) / expected_plasma_freq
+fig = Figure(size = (1000, 400))
+ax = Axis(
+    fig[1, 1],
+    title = "Electric Field Energy Spectrum",
+    xlabel = "Frequency",
+    ylabel = "Amplitude",
+)
+lines!(ax, freqs[1:cutoff_index], freq_amps[1:cutoff_index])
+vlines!(ax, [measured_plasma_freq * 2])
+fig
 
-boltzmann_constant = 1.381e-23
-dx^2 * number_density * elec_charge^2 / epsilon_0 / boltzmann_constant
+relative_error = (measured_plasma_freq - expected_plasma_freq) / expected_plasma_freq
 
-dx^2 * number_density * elec_charge / epsilon_0
-
-function measure_plasma_frequency(number_density, temperature, wavenumber)
+function measure_plasma_frequency(number_density, wavenumber, normalized_timestep = 0.01)
     sim_length = 1.0
-
     num_cells = 32
     dx = sim_length / num_cells
 
-    num_macroparticles = 320
+    num_macroparticles = 10 * num_cells
     particles_per_macro = number_density * sim_length / num_macroparticles
 
     perturb_amplitude = 1e3
     elec_mass = 9e-31
-    boltzmann_constant = 1.381e-23
-    thermal_velocity = sqrt(3 * boltzmann_constant * temperature / elec_mass)
-
     positions = collect(0:num_macroparticles-1) ./ num_macroparticles
     momentums =
-        (particles_per_macro * elec_mass) .*
-        (perturb_amplitude .* sin.(positions .* wavenumber) .+ thermal_velocity .* randn.())
+        (particles_per_macro * elec_mass * perturb_amplitude) .*
+        sin.(positions .* wavenumber)
 
     electrons = ParticleInCell.electrons(positions, momentums, particles_per_macro)
 
     grid = UniformCartesianGrid((0.0,), (sim_length,), (num_cells,), (true,))
 
-    dt = 5e-11
+    epsilon_0 = 8.8e-12
+    elec_charge = 1.6e-19
+    expected_plasma_freq = sqrt(number_density * elec_charge^2 / elec_mass / epsilon_0)
+    dt = normalized_timestep / expected_plasma_freq
     sim, fields = create_electrostatic_simulation(grid, [electrons], dt)
     Enode = fields[:Enode]
 
-    n_steps = 1000
+    # We want to simulate the same length of physical time for each simulation,
+    # so we need to have more steps when the timestep is shorter.
+    n_steps = round(Int, 100 / normalized_timestep)
     electric_field_energy = Vector{Float64}(undef, n_steps)
 
     epsilon_0 = 8.8e-12
@@ -157,20 +178,44 @@ function measure_plasma_frequency(number_density, temperature, wavenumber)
     plasma_freq = abs(max_freq / 2)
 
     return plasma_freq
-end
+end;
 
-temperatures = [0, 0.1, 1, 10]
-measure_plasma_frequency.(1e14, temperatures, 1 / 2 * pi)
+wavenumbers = 2 * pi .* [2, 4, 8, 16]
+plasma_freqs = measure_plasma_frequency.(1e14, wavenumbers, 0.001)
 
-function warm_plasma_freq(number_density, temperature, wavenumber)
-    epsilon_0 = 8.8e-12
-    elec_charge = 1.6e-19
-    elec_mass = 9e-31
-    boltzmann_constant = 1.381e-23
-    square_plasma_freq = number_density * elec_charge^2 / elec_mass / epsilon_0
-    correction_factor = boltzmann_constant * temperature * wavenumber^2 / elec_mass
-    return sqrt(square_plasma_freq + correction_factor)
-end
-warm_plasma_freq.(1e14, temperatures, 1 / 2 * pi)
+dx = 1 / 32
+normalized_wavenumbers = wavenumbers .* dx
+relative_errors = abs.(plasma_freqs ./ expected_plasma_freq .- 1)
+
+fig = Figure(size = (1000, 400))
+ax = Axis(
+    fig[1, 1],
+    title = "Frequency error",
+    xlabel = L"k \Delta x",
+    ylabel = L"\left|\frac{\omega}{\omega_p} - 1\right|",
+    xscale = log10,
+    yscale = log10,
+)
+scatter!(ax, normalized_wavenumbers, relative_errors)
+fig
+
+using LsqFit
+
+model(x, p) = p[1] .+ x .* p[2]
+fit = curve_fit(model, log10.(normalized_wavenumbers), log10.(relative_errors), [0.0, 2.0])
+params = coef(fit)
+
+lines!(
+    ax,
+    normalized_wavenumbers,
+    (10^params[1]) .* normalized_wavenumbers .^ params[2],
+    color = :red,
+)
+fig
+
+corrected_plasma_freqs =
+    plasma_freqs ./ (1 .- (10^params[1]) .* normalized_wavenumbers .^ params[2])
+corrected_relative_errors =
+    (corrected_plasma_freqs .- expected_plasma_freq) ./ expected_plasma_freq
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
