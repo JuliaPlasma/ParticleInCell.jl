b7b3bdb12a2ea0251ad817f4dc1d33385af3aeb2
Luke Adams <luke@lukeclydeadams.com>
2024-12-29 11:34:47 -0700

    Add functionality for quiet start
    Currently, this only really works for 1d1v simulations, but one could
    probably generate multiple quiet start velocities, and permute them
    according to bit reversed sequences with different (prime) bases. The
    positions would also need to be bit reversed differently in each
    dimension. This would naively require the number of macroparticles to
    have a very specific prime decomposition, but maybe the permutation
    could be more clever?

--- src/species/species_test.jl
+++ src/species/species_test.jl
@@ -1,5 +1,6 @@
 @testitem "Species utilities" tags = [:unit] begin
     using StaticArrays
+    using Distributions
 
     @test electrons([1.0 2.0 3.0], [4.0 5.0 6.0], [1.0, 2.0, 3.0]) ==
           VariableWeightSpecies{1,1,Float64}(
@@ -36,4 +37,11 @@
         1.602176634e-19,
         9.1093837015e-31,
     )
+
+    dist = Normal(0.0, 1.0)
+    @test ParticleInCell.quiet_velocities(4, dist) ≈ [-1.1503493803760087, -0.3186393639643751, 0.3186393639643751, 1.1503493803760087]
+
+    # See Birdsall and Langdon, Table 16.5a
+    @test ParticleInCell.bit_reversed_sequence(6) == [0, 1/2, 1/4, 3/4, 1/8, 5/8]
+    @test ParticleInCell.bit_reversed_sequence(6, 3) == [0, 1/3, 2/3, 1/9, 4/9, 7/9]
 end
