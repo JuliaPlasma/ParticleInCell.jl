f59fecedbbf98d7fbf548c9262baef09881141e2
Luke Adams <luke@lukeclydeadams.com>
2024-12-29 12:27:09 -0700

    fix formatting

--- src/species/species_test.jl
+++ src/species/species_test.jl
@@ -39,9 +39,11 @@
     )
 
     dist = Normal(0.0, 1.0)
-    @test ParticleInCell.quiet_velocities(4, dist) ≈ [-1.1503493803760087, -0.3186393639643751, 0.3186393639643751, 1.1503493803760087]
+    @test ParticleInCell.quiet_velocities(4, dist) ≈
+          [-1.1503493803760087, -0.3186393639643751, 0.3186393639643751, 1.1503493803760087]
 
     # See Birdsall and Langdon, Table 16.5a
-    @test ParticleInCell.bit_reversed_sequence(6) == [0, 1/2, 1/4, 3/4, 1/8, 5/8]
-    @test ParticleInCell.bit_reversed_sequence(6, 3) == [0, 1/3, 2/3, 1/9, 4/9, 7/9]
+    @test ParticleInCell.bit_reversed_sequence(6) == [0, 1 / 2, 1 / 4, 3 / 4, 1 / 8, 5 / 8]
+    @test ParticleInCell.bit_reversed_sequence(6, 3) ==
+          [0, 1 / 3, 2 / 3, 1 / 9, 4 / 9, 7 / 9]
 end
